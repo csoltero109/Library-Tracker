cb558d1ae6fae8342014bc94827f8ca44591f028
unknown <chrissoltero109@gmail.com>
2021-03-26 13:18:37 -0700

    added tables to view basic infor

--- index.js
+++ index.js
@@ -1,30 +1,25 @@
 // https://www.googleapis.com/books/v1/volumes/XA1evgEACAAJ
-
-// https://www.googleapis.com/books/v1/volumes/XA1evgEACAAJ
-/*function cleanISBN(isbn){
-	var newISBN = isbn.split('-').join('');
-	return newISBN;
-}*/
+// request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=isbn:9781566199094");
 
 function getISBN(){
 	var isbn = document.getElementById("isbn").value;
-	//var cleanISBN = cleanISBN(isbn);
+	var correctedISBN = cleanISBN(isbn);
 	let request = new XMLHttpRequest();
-	request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn);
-	// request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=isbn:9781566199094");
+	request.open("GET", "https://www.googleapis.com/books/v1/volumes?q=isbn:" + correctedISBN);
 	request.send();
 	request.onload = () => {
 		console.log(request);
 		try{
 			if(request.status == 200){
 				const json = JSON.parse(request.response);
-				
+				var arr = determineISBNTypes(json.items[0].volumeInfo.industryIdentifiers[0].identifier, json.items[0].volumeInfo.industryIdentifiers[1].identifier);
 				var book = {
 					title: json.items[0].volumeInfo.title,
 					bookImage: json.items[0].volumeInfo.imageLinks.thumbnail,
 					description: json.items[0].volumeInfo.description,
-					isbn10: json.items[0].volumeInfo.industryIdentifiers[1].identifier,
-					isbn13: json.items[0].volumeInfo.industryIdentifiers[0].identifier,
+					isbn10: arr[0],
+					isbn13: arr[1],
+					authors: json.items[0].volumeInfo.authors,
 					selflink: json.items[0].selfLink
 				};
 				//displayInfo(book);
@@ -36,9 +31,38 @@ function getISBN(){
 			}
 		}
 		catch(err){
-			document.getElementById("nothingReturned").innerHTML = "The ISBN number entered has returned nothing...";
+			try{
+				if(request.status == 200){
+				const json = JSON.parse(request.response);
+				//var arr = determineISBNTypes(json.items[0]);
+				var book = {
+					title: json.items[0].volumeInfo.title,
+					bookImage: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png",
+					description: json.items[0].volumeInfo.description,
+					isbn10: json.items[0].volumeInfo.industryIdentifiers[0].identifier,
+					isbn13: json.items[0].volumeInfo.industryIdentifiers[1].identifier,
+					authors: json.items[0].volumeInfo.authors,
+					selflink: json.items[0].selfLink
+				};
+				console.log(book);
+				
+				
+				//displayInfo(book);
+				addRow(book);
+				console.log(JSON.parse(request.response));
+			}
+			}
+			catch(err){
+				
+			}
+			//document.getElementById("nothingReturned").innerHTML = "The ISBN number entered has returned nothing...";
 		}
 	}
+}
+
+function cleanISBN(isbn){
+	var newISBN = isbn.split('-').join('');
+	return newISBN;
 }
 
 function displayInfo(book){
@@ -51,7 +75,6 @@ function displayInfo(book){
 	document.getElementById("selflink").innerHTML = "Click here for JSON";	
 }
 
-
 function addRow(book) {
   var table = document.getElementById("bookTable");
   var row = table.insertRow(1);
@@ -60,13 +83,40 @@ function addRow(book) {
   var description = row.insertCell(2);
   var isbn10 = row.insertCell(3);
   var isbn13 = row.insertCell(4);
-  var selfLink = row.insertCell(5);
-  title.innerHTML = book.title;
+  var authors = row.insertCell(5);
+  title.innerHTML = '<a id="' + book.isbn13 + '" class="link" href="' + book.selflink + '">' + book.title + '</a>';
   image.innerHTML = '<img style="display:block;" src="' + book.bookImage + '" />';
   description.innerHTML = book.description;
   description.style.width = '25%';
   isbn10.innerHTML = book.isbn10;
   isbn13.innerHTML = book.isbn13;
-  selfLink.innerHTML = '<a id="' + book.title + '" class="link" href="' + book.selflink + '">JSON</a>';
-  //selfLink.href = book.selflink
+  authors.innerHTML = book.authors;
 }
+
+function determineISBNTypes(length0, length1){
+	var isbnLengths = new Array(2);
+	if(length0.length > length1.length){
+		isbnLengths[1] = length0; //Obj at index 1 is ISBN-13
+		isbnLengths[0] = length1; //Obj at index 0 is ISBN-10
+	}
+	else{
+		isbnLengths[1] = length1; //Obj at index 1 is ISBN-13
+		isbnLengths[0] = length0; //Obj at index 0 is ISBN-10
+	}
+	return isbnLengths;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
